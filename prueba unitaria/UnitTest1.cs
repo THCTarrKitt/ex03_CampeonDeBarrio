275f9d73b8f017b2343f70d6af361f5d2bbf1eae
hp <hp@LAPTOP-D5IS08BH>
2022-05-19 23:09:23 -0500

    Agregar archivos de proyecto.

---
+++ prueba unitaria/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace prueba_unitaria
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
